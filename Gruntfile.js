d11fa99edf0a010da44194d2c833b945f0bb655a
Dan Stocker <dan@kwaia.com>
2013-07-22 09:13:07 +0200

    initial commit, separates CLI function from old radiant (lightstore) repo

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+/*jshint node:true */
+module.exports = function (grunt) {
+    "use strict";
+
+    var params = {
+        files: [
+            'js/namespace.js',
+            'js/Rjson.js',
+            'js/KeyValueStore.js',
+            'js/exports.js'
+        ],
+
+        test: [
+            'js/jsTestDriver.conf'
+        ],
+
+        globals: {
+            dessert: true,
+            troop  : true,
+            sntls  : true
+        }
+    };
+
+    // invoking common grunt process
+    require('common-gruntfile')(grunt, params);
+};

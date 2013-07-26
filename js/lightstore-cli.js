aa0e1922610cbb380e9e40ffe52f75400c949714
Dan Stocker <dan@kwaia.com>
2013-07-26 09:45:39 +0200

    turns CLI into prompt-based

--- js/lightstore-cli.js
+++ js/lightstore-cli.js
@@ -1,115 +1,68 @@
-/**
- * Command line tool for accessing RJSON databases.
- */
 /*jshint node:true */
 (function () {
     'use strict';
 
     var $path = require('path'),
+        $readline = require('readline').createInterface({
+            input : process.stdin,
+            output: process.stdout
+        }),
         lightstore = /** @type {lightstore} */require('lightstore'),
-        stdout = process.stdout,
-        argv = process.argv,
-        fileName = argv[2],
-        fileExt = $path.extname(fileName),
-        isLightStore = fileExt === '.ls',
-        store = isLightStore ?
-            lightstore.KeyValueStore.create(fileName) :
-            lightstore.Rjson.create(fileName),
-        command = argv[3],
-        args = argv.slice(4);
-
-    //////////////////////////////
-    // Utils
+        fileName = process.argv[2],
+        command = process.argv[3],
+        ls; // datastore object
 
     /**
-     * Outputs an error message to stdio.
-     * @static
-     * @param err {Error|string}
+     * One iteration in the readline-loop.
+     * Processes a single JvaScript command.
+     * @param {string} answer
      */
-    function error(err) {
-        if (typeof err === 'string') {
-            err = new Error(err);
+    function prompt(answer) {
+        var message;
+
+        // evaluating command
+        /*jshint evil:true */
+        if (answer) {
+            try {
+                message = eval(answer);
+                if (typeof message === 'object') {
+                    message = JSON.stringify(message, null, 2);
+                }
+            } catch (e) {
+                message = e.toString();
+            }
+
+            process.stdout.write(message + "\n");
         }
-        stdout.write(err.toString() + "\n");
+
+        // print next prompt
+        if (!command) {
+            $readline.question("> ", prompt);
+        } else {
+            process.exit();
+        }
     }
 
     /**
-     * Outputs a message to stdio.
-     * @static
-     * @param message {string}
+     * Opens LightStore file.
+     * @param {string} fileName
      */
-    function ok(message) {
-        stdout.write(message + "\n");
+    function open(fileName) {
+        ls = lightstore.PersistedTree.create(fileName)
+            .read(prompt.bind(null, command));
     }
 
-    //////////////////////////////
-    // Parameter check
-
-    if (!fileName) {
-        ok("Usage: lightstore fileName [command] [data]");
+    /**
+     * Shorthand for exiting from CLI.
+     */
+    function exit() {
         process.exit();
     }
 
-    //////////////////////////////
-    // Datastore access
-
-    switch (command) {
-    case 'compact':
-        /**
-         * Compacting database.
-         */
-        store.compact(function (err) {
-            if (err) {
-                error(err);
-            } else {
-                ok("Datastore compacted.");
-            }
-        });
-        break;
-
-    case 'write':
-        if (!args.length) {
-            break;
-        }
-
-        if (isLightStore) {
-            // file is a lightstore file
-            args[0] = args[0].toPath();
-            try {
-                args[1] = JSON.parse(args[1]);
-            } catch (e) {
-                // if parsing fails, we'll still write it as string
-                error("Invalid JSON");
-            }
-        } else {
-            // file is a plain RJSON
-            try {
-                args[0] = JSON.parse(args[0]);
-            } catch (e) {
-                error("Invalid JSON");
-                break;
-            }
-        }
-
-        args.push(function () {
-            ok("Data written.");
-        });
-
-        store.write.apply(store, args);
-        break;
-
-    default:
-    case 'read':
-        /**
-         * Reading database and outputs contents.
-         */
-        store.read(function (err, data) {
-            if (err) {
-                error(err);
-            } else {
-                ok(JSON.stringify(data, null, 2));
-            }
-        });
-        break;
+    // initiating prompt either directly or by opening file
+    if (fileName) {
+        open(fileName);
+    } else {
+        prompt();
     }
 }());
